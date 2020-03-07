f6082fe9c1719afc5e072d8eac8dc0f58ed2553d
miki <kergeodeta@gmail.com>
2020-03-07 19:36:09 +0100

    create constructor like function for DateTime creation

--- types/datetime.go
+++ types/datetime.go
@@ -1,10 +1,27 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type DateTime struct {
 	Date
 	Time
+}
+
+func CreateDateTime(year, month, day, hour, minute, second int) *DateTime {
+	return &DateTime{
+		Date: Date{
+			Year:        year,
+			MonthOfYear: Month(month),
+			DayOfMonth:  day,
+		},
+		Time: Time{
+			Hours:   hour,
+			Minutes: minute,
+			Seconds: second,
+		},
+	}
 }
 
 func (dt DateTime) String() string {
