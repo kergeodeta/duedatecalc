808a29f3b81b7c8ca4fb0cf34dfbe8b67fc7fff4
miki <kergeodeta@gmail.com>
2020-03-07 21:39:23 +0100

    implements copy method

--- types/datetime.go
+++ types/datetime.go
@@ -24,6 +24,21 @@ func CreateDateTime(year, month, day, hour, minute, second int) *DateTime {
 	}
 }
 
+func (dt DateTime) Copy() DateTime {
+	return DateTime{
+		Date: Date{
+			Year:        dt.Year,
+			MonthOfYear: dt.MonthOfYear,
+			DayOfMonth:  dt.DayOfMonth,
+		},
+		Time: Time{
+			Hours:   dt.Hours,
+			Minutes: dt.Minutes,
+			Seconds: dt.Seconds,
+		},
+	}
+}
+
 func (dt DateTime) String() string {
 	return fmt.Sprintf("%s %s", dt.Date, dt.Time)
 }
