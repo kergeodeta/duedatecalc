9a6f7772f1d3c7207004970c93f7c0ff4ab097e1
miki <kergeodeta@gmail.com>
2020-03-07 15:06:01 +0100

    implements time and datetime types

--- types/datetime.go
+++ types/datetime.go
@@ -1,6 +1,12 @@
 package types
 
+import "fmt"
+
 type DateTime struct {
 	Date
 	Time
+}
+
+func (dt DateTime) String() string {
+	return fmt.Sprintf("%s %s", dt.Date, dt.Time)
 }
--- types/time.go
+++ types/time.go
@@ -1,7 +1,13 @@
 package types
 
+import "fmt"
+
 type Time struct {
 	Hours   int
 	Minutes int
 	Seconds int
+}
+
+func (t Time) String() string {
+	return fmt.Sprintf("%02d:%02d:%02d", t.Hours, t.Minutes, t.Seconds)
 }
